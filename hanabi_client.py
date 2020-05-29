372d2d6f15da851812139dd1cf888c06a267dfa1
Ryan Piersma <rrp17@duke.edu>
2020-05-29 14:01:54 -0400

    Client and thread manager can now interact to create data connections

--- hanabi_client.py
+++ hanabi_client.py
@@ -11,70 +11,75 @@ from send_codes import SendCode
 import sys  # In order to terminate the program
 import random
 
+        
 
-class Client:
-    def __init__(self, server_ip, server_port):
-        self.server_ip = server_ip
-        self.server_port = server_port
+def send_data_port(server_ip, server_port):
 
-    def send_data_port(self):
+    # Create server socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((server_ip, server_port))
+    print("Successful connection to server made")
 
-        # Create server socket
-        clientSocket = socket(AF_INET, SOCK_STREAM)
-        clientSocket.connect((self.server_ip, self.server_port))
-        print("Successful connection to server made")
-
-        try:
-            print("Waiting to receive server code...")
-            serverMessage = clientSocket.recv(4).decode()
-            
-            if serverMessage == SendCode.INDICATE_PLAYER_ONE.value:
-                to_server = input('Welcome to Hana(N)bi! How many players for your game?\n')
-                
-                numIterations = 0
-                while (serverMessage != SendCode.INDICATE_VALID_PLAYERNUM.value):
-                    if numIterations != 0:
-                        to_server = input("Sorry, enter a number of players between 2 and 5!\n")
-                    clientSocket.send(to_server.encode())
-                    print('Thank you!')
-                    serverMessage = clientSocket.recv(4).decode()
-                    numIterations = numIterations + 1
-            else:
-                print('Welcome to your game of Hana(N)bi!\n')
-             
-            serverMessage = clientSocket.recv(4).decode()
-            while (serverMessage != SendCode.SERVER_REQUEST_DATA_PORT.value):
-                serverMessage = clientSocket.recv(4).decode()
-                
-            serverMessage = ''
+    try:
+        print("Waiting to receive server code...")
+        serverMessage = clientSocket.recv(4).decode()
+    
+        if serverMessage == SendCode.INDICATE_PLAYER_ONE.value:
+            to_server = input('Welcome to Hana(N)bi! How many players for your game?\n')
+        
             numIterations = 0
-            while (serverMessage != SendCode.SERVER_RECEIVED_DATA_PORT.value):
-                if (numIterations != 0):
-                    print("Server failed to receive data port! Retrying...")
-                dataPort = self.get_available_port()
-                clientSocket.send(str(dataPort).encode())  
-                print("Data port sent to server!")
+            while (serverMessage != SendCode.INDICATE_VALID_PLAYERNUM.value):
+                if numIterations != 0:
+                    to_server = input("Sorry, enter a number of players between 2 and 5!\n")
+                clientSocket.send(to_server.encode())
+                print('Thank you!')
+                serverMessage = clientSocket.recv(4).decode()
+                numIterations = numIterations + 1
+        else:
+            print('Welcome to your game of Hana(N)bi!\n')
+             
+        serverMessage = clientSocket.recv(4).decode()
+        while (serverMessage != SendCode.SERVER_REQUEST_DATA_PORT.value):
                 serverMessage = clientSocket.recv(4).decode()
                 
-            clientSocket.send(SendCode.CLIENT_CLOSE_SOCKET.value.encode())
-            clientSocket.close()
-            return dataPort
+        serverMessage = ''
+        numIterations = 0
+        while (serverMessage != SendCode.SERVER_RECEIVED_DATA_PORT.value):
+            if (numIterations != 0):
+                print("Server failed to receive data port! Retrying...")
+            dataPort = get_available_port()
+            clientSocket.send(str(dataPort).encode())  
+            print("Data port sent to server!")
+            serverMessage = clientSocket.recv(4).decode()
+                
+        clientSocket.send(SendCode.CLIENT_CLOSE_SOCKET.value.encode())
+        clientSocket.close()
+        return dataPort
 
-        except EOFError:
-            clientSocket.close()
-            
-    def get_available_port(self):
+    except EOFError:
+        clientSocket.close()
+          
+def open_data_socket(dataPort):
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.bind(('', dataPort))
+    dataSocket.listen(1)
     
-        portAvailable = 0
-        portProspect = 0
+    dataSocket.accept()
+    print("Successful data connection created")
+
+
+def get_available_port():
     
-        while (portAvailable == 0):
-            portProspect = random.randint(1024,65536)
-            portLocation = ("127.0.0.1", portProspect)
-            testSocket = socket(AF_INET, SOCK_STREAM)
-            portAvailable = testSocket.connect_ex(portLocation)
+    portAvailable = 0
+    portProspect = 0
+    
+    while (portAvailable == 0):
+        portProspect = random.randint(1024,65536)
+        portLocation = ("127.0.0.1", portProspect)
+        testSocket = socket(AF_INET, SOCK_STREAM)
+        portAvailable = testSocket.connect_ex(portLocation)
             
-        return portProspect
+    return portProspect
 
 if __name__ == '__main__':
     server_ip = input("Enter server IP (default localhost): ")
@@ -87,5 +92,8 @@ if __name__ == '__main__':
     else:
         server_port = sys.argv[1]
         
-    client = Client(server_ip, int(server_port))
-    client.send_data_port()
+    print("Client will connect to server and tell it its data port\n\n")
+    dataPort = send_data_port(server_ip, int(server_port))
+    
+    print("Client will open socket for its data port and alert when connected to server")
+    open_data_socket(dataPort)
--- thread_manager.py
+++ thread_manager.py
@@ -9,20 +9,19 @@ import join_phase as jp
 import threading
 from socket import *
 
-def establish_data_connection(data_port): #Call this fcn w threading
-    baseSocket = socket(AF_INET, SOCK_STREAM)
-    baseSocket.bind(('',data_port))
-    baseSocket.listen(1)
+def establish_data_connection(client_ip, data_port): #Call this fcn w threading
+
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.connect((client_ip, data_port))
     
-    dataSocket, addr = baseSocket.accept()
     print("Data socket on port " + str(data_port) + " successfully connected")
     dataSocket.send("Welcome to your data connection".encode())
-    
     dataSocket.close()
     
-
+  
 if __name__ == "__main__":
-    data_ports = jp.join_phase()
-    #establish_data_connection(data_ports)
+    (client_ips, data_ports) = jp.join_phase()
+    for i in range(len(client_ips)):
+        establish_data_connection(client_ips[i], data_ports[i])
 
 #x = threading.Thread(target=thread_function, args=(1,))

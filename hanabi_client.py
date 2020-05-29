2d4b94575d8dee410446a1a4a92a69fd02cccf91
Ryan Piersma <rrp17@duke.edu>
2020-05-28 23:19:35 -0400

    Renamed 'simple_client' to 'hanabi_client' as functionality is and will be extended

--- hanabi_client.py
+++ hanabi_client.py
@@ -9,6 +9,7 @@ Created on Mon May 11 21:33:05 2020
 from socket import *
 from send_codes import SendCode
 import sys  # In order to terminate the program
+import random
 
 
 class Client:
@@ -16,7 +17,7 @@ class Client:
         self.server_ip = server_ip
         self.server_port = server_port
 
-    def run(self):
+    def send_data_port(self):
 
         # Create server socket
         clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -35,17 +36,45 @@ class Client:
                     if numIterations != 0:
                         to_server = input("Sorry, enter a number of players between 2 and 5!\n")
                     clientSocket.send(to_server.encode())
-                    print('Thank you! Please wait for your data port number')
+                    print('Thank you!')
                     serverMessage = clientSocket.recv(4).decode()
                     numIterations = numIterations + 1
             else:
-                print('Welcome to your game of Hana(N)bi! Please wait for your data port\n')
+                print('Welcome to your game of Hana(N)bi!\n')
+             
+            serverMessage = clientSocket.recv(4).decode()
+            while (serverMessage != SendCode.SERVER_REQUEST_DATA_PORT.value):
+                serverMessage = clientSocket.recv(4).decode()
                 
-            dataPort = clientSocket.recv(8).decode()
-            print("Going to connect on port " + dataPort)    
+            serverMessage = ''
+            numIterations = 0
+            while (serverMessage != SendCode.SERVER_RECEIVED_DATA_PORT.value):
+                if (numIterations != 0):
+                    print("Server failed to receive data port! Retrying...")
+                dataPort = self.get_available_port()
+                clientSocket.send(str(dataPort).encode())  
+                print("Data port sent to server!")
+                serverMessage = clientSocket.recv(4).decode()
+                
+            clientSocket.send(SendCode.CLIENT_CLOSE_SOCKET.value.encode())
+            clientSocket.close()
+            return dataPort
 
         except EOFError:
             clientSocket.close()
+            
+    def get_available_port(self):
+    
+        portAvailable = 0
+        portProspect = 0
+    
+        while (portAvailable == 0):
+            portProspect = random.randint(1024,65536)
+            portLocation = ("127.0.0.1", portProspect)
+            testSocket = socket(AF_INET, SOCK_STREAM)
+            portAvailable = testSocket.connect_ex(portLocation)
+            
+        return portProspect
 
 if __name__ == '__main__':
     server_ip = input("Enter server IP (default localhost): ")
@@ -59,43 +88,4 @@ if __name__ == '__main__':
         server_port = sys.argv[1]
         
     client = Client(server_ip, int(server_port))
-    client.run()
-    def __init__(self, server_ip, server_port):
-        self.server_ip = server_ip
-        self.server_port = server_port
-
-    def run(self):
-
-        # Create server socket
-
-        # Add your code here
-        clientSocket = socket(AF_INET, SOCK_STREAM)
-        clientSocket.connect((self.server_ip, self.server_port))
-
-        # Get input with python raw_input() or input()
-
-        # Add your code here
-        # Hint:
-        # try:
-        #     from_client = raw_input("Enter message: \n")
-        # ...
-        # ...
-        try:
-            while to_server.lower().strip() != 'bye':
-                # send and receive message
-
-                # Add your code here
-                clientSocket.send(to_server.encode())
-                from_server = clientSocket.recv(512).decode()
-                print(from_server)   # show in terminal
-
-                # Get input again
-
-                # Add your code here
-                try:
-                    to_server = input('Enter message: \n')
-                except EOFError:
-                    clientSocket.close()  # close the connection
-                    break
-        except EOFError:
-            clientSocket.close()
+    client.send_data_port()

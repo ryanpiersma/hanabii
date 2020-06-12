e9370f21b035e5aa189f4eec441de1c8cee951a1
Ryan Piersma <rrp17@duke.edu>
2020-06-11 23:23:20 -0400

    Skeleton of thread manager functions created

--- thread_manager.py
+++ thread_manager.py
@@ -7,9 +7,22 @@ Created on Wed May 20 17:17:34 2020
 
 import join_phase as jp
 import threading
+import queue
 from socket import *
 
-def establish_data_connection(client_ip, data_port): #Call this fcn w threading
+threadActivationList = [] #Populate this w condition variables
+
+def create_condition_variables(num_players):
+    return 0
+
+
+def game_manager():
+    return 0
+    
+def game_player():
+    return 0
+    
+def establish_data_connection(client_ip, data_port): #Call this fcn thru game_manager??
 
     dataSocket = socket(AF_INET, SOCK_STREAM)
     dataSocket.connect((client_ip, data_port))
@@ -21,7 +34,6 @@ def establish_data_connection(client_ip, data_port): #Call this fcn w threading
   
 if __name__ == "__main__":
     (client_ips, data_ports) = jp.join_phase()
-    for i in range(len(client_ips)):
-        establish_data_connection(client_ips[i], data_ports[i])
+    
 
 #x = threading.Thread(target=thread_function, args=(1,))
